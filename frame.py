a4943421e0f1900c86f35e344cec258af4a62124
S4nop <rshtiger@naver.com>
2019-12-02 19:50:01 +0900

    make class to prepare multithread

--- frame.py
+++ frame.py
@@ -3,46 +3,47 @@ import cv2
 import subprocess
 import os
 
-global imgList
-global imgNum
+class PictureViewer:
+    imgList = []
+    imgNum = 0
 
-def updateImageList():
-    path = "./img"
-    return os.listdir(path);
 
-def getFrameSize():
-    cmd = ['xrandr']
-    cmd2 = ['grep', '*']
-    p = subprocess.Popen(cmd, stdout=subprocess.PIPE)
-    p2 = subprocess.Popen(cmd2, stdin=p.stdout, stdout=subprocess.PIPE)
-    p.stdout.close()
+    def updateImageList(self):
+        path = "./img"
+        self.imgList =  os.listdir(path);
 
-    resolution_string, junk = p2.communicate()
-    resolution = resolution_string.split()[0]
-    width, height = resolution.decode('utf-8').split("x")
-    return width, height 
+    def getFrameSize(self):
+        cmd = ['xrandr']
+        cmd2 = ['grep', '*']
+        p = subprocess.Popen(cmd, stdout=subprocess.PIPE)
+        p2 = subprocess.Popen(cmd2, stdin=p.stdout, stdout=subprocess.PIPE)
+        p.stdout.close()
 
-def showPicture():
-    width, height = getFrameSize()
-    imgCV = cv2.imread("./img/" + imgList[imgNum])
-    imgCV = cv2.resize(imgCV, (int(width), int(height)), interpolation = cv2.INTER_AREA)
-    cv2.setMouseCallback("res", showNextImg)
-    cv2.imshow("res", imgCV)
-    cv2.waitKey(100)
+        resolution_string, junk = p2.communicate()
+        resolution = resolution_string.split()[0]
+        width, height = resolution.decode('utf-8').split("x")
+        return width, height 
 
-def showNextImg(event, x, y, flags, param):
-    global imgList, imgNum
-    if event == cv2.EVENT_LBUTTONDOWN:
-        if imgNum == len(imgList) - 1:
-            imgNum = 0
-            print("imgNum = 0")
-        else:
-            imgNum = imgNum + 1
-            print(imgNum)
+    def showPicture(self):
+        width, height = self.getFrameSize()
+        imgCV = cv2.imread("./img/" + self.imgList[self.imgNum])
+        imgCV = cv2.resize(imgCV, (int(width), int(height)), interpolation = cv2.INTER_AREA)
+        cv2.setMouseCallback("res", self.showNextImg)
+        cv2.imshow("res", imgCV)
+        cv2.waitKey(10)
 
+    def showNextImg(self, event, x, y, flags, param):
+        if event == cv2.EVENT_LBUTTONDOWN:
+            if self.imgNum == len(self.imgList) - 1:
+                self.imgNum = 0
+                print("imgNum = 0")
+            else:
+                self.imgNum = self.imgNum + 1
+                print(self.imgNum)
 
 if __name__=="__main__":
-    imgList = updateImageList()
-    imgNum = 0
+    pv = PictureViewer()
+    pv.updateImageList()
     while(1):
-        showPicture()
+        pv.showPicture()
+
